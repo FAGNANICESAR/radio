44ffd0788fee6844defc5abe280cafbbf5d4b692
FAGNANICESAR <59416266+FAGNANICESAR@users.noreply.github.com>
2025-08-21 19:24:04 -0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    radio.sendString("Micro Chat")
+})
+radio.onReceivedString(function on_received_string(receivedString: string) {
+    basic.showString(receivedString)
+})
